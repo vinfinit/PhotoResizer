71897fd59fb93764c2ff5b354b5327a1d98fe2fe
vinfinit <artemenko.vladimi@gmail.com>
2016-08-02 21:48:56 +0300

    Photos from folder to other folder

--- photoResizer/Program.cs
+++ photoResizer/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.IO;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
@@ -16,19 +17,39 @@ namespace photoResizer
         static void Main(string[] args)
         {
             var config = new ConfigManager();
-            PhotoManager.Resize("C:\\Users\\uladzimir_artsemenka\\Downloads\\vovchik.jpg", "C:\\Users\\uladzimir_artsemenka\\Downloads\\vovchik_resized1.jpg");
-            Console.ReadLine();
+            var photoManager = new PhotoManager(config);
+            
+            photoManager.Resize();
         }
     }
 
-    static class PhotoManager
+    class PhotoManager
     {
-        public static void Resize(string inputPath, string outputPath)
+        private string OutputFolder { get; }
+        private string InputFolder { get; }
+
+        public PhotoManager(ConfigManager config)
+        {
+            OutputFolder = config.OutputFolder;
+            InputFolder = config.InputFolder;
+        }
+
+        public void Resize(string inputPath, string outputPath)
         {
             var captureImage = new Image<Bgr, byte>(inputPath);
             var resizedImage = captureImage.Resize(600, 600, Inter.Cubic);
             resizedImage.Save(outputPath);
         }
+
+        public void Resize()
+        {
+            var photosList = Directory.GetFiles(InputFolder, "*.*").ToList();
+            foreach (var photo in photosList)
+            {
+                Resize(photo, $"{OutputFolder}{Path.GetFileNameWithoutExtension(photo)}{"_1"}{Path.GetExtension(photo)}");
+            }
+        }
+       
     }
 
 }
