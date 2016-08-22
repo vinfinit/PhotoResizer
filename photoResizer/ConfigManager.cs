dc767985e119faae251a8ef7d09a85849b126fb2
vinfinit <artemenko.vladimi@gmail.com>
2016-08-22 21:46:36 +0300

    Resolution to class

--- photoResizer/ConfigManager.cs
+++ photoResizer/ConfigManager.cs
@@ -33,27 +33,4 @@ namespace photoResizer
             Resolutions = Array.ConvertAll(resolutions, (i) => new Resolution(i)).ToList();
         }
     }
-
-    class Resolution
-    {
-        public int Width { get; private set; }
-        public int Height { get; private set; }
-
-        public Resolution(int width, int height)
-        {
-            Setup(width, height);
-        }
-
-        public Resolution(string resolution)
-        {
-            var size = Array.ConvertAll(resolution.Split('x'), int.Parse);
-            Setup(size[0], size[1]);
-        }
-
-        private void Setup(int width, int height)
-        {
-            Width = width;
-            Height = height;
-        }
-    }
 }
---
+++ photoResizer/Resolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace photoResizer
+{
+    class Resolution
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Resolution(int width, int height)
+        {
+            Setup(width, height);
+        }
+
+        public Resolution(string resolution)
+        {
+            var size = Array.ConvertAll(resolution.Split('x'), int.Parse);
+            Setup(size[0], size[1]);
+        }
+
+        private void Setup(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
